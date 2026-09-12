aed586463d17a8aa03d3bc32eb78d77636c1824f
Polina18-user <pd-fedkina@yandex.ru>
2026-09-12 08:49:09 +0300

    Add files via upload

---
+++ БД/задание 7.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM simpledb.users
+LIMIT 0, 1000
+
+-- Date: 2026-09-12 08:48
+*/
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (1,'Petr','petr-12345@yandex.ru','M','1990-09-09','12345',1.3,'2026-09-10 19:14:40');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (2,'Fedor','fEdOr-1111@yandex.ru','M','1999-10-09','67890',2.9,'2026-09-10 19:14:40');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (3,'Igor','igor-igoryashka@yandex.ru','M','2000-10-09','13579',1.7,'2026-09-10 19:14:40');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (4,'Ekaterina','ekaterina.petrova@outlook.com','F','2000-02-11','145789',1.123,'2026-09-10 19:28:41');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (5,'Paul','paul@superpochta.ru','M','1998-08-12','123789',1,'2026-09-10 19:28:41');
