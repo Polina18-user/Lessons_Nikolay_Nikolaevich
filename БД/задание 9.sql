287b0d32f7f4ca3a32802d6fd0944e8336c008a9
Polina18-user <pd-fedkina@yandex.ru>
2026-09-13 10:31:40 +0300

    Add files via upload

---
+++ БД/задание 9.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM simpledb.resume
+LIMIT 0, 1000
+
+-- Date: 2026-09-13 10:20
+*/
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (1,1,'abcd','mature','2026-09-13 07:03:04');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (2,2,'efghi','working in Figma','2026-09-13 07:04:09');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (3,3,'jklmn','solving Maths problems','2026-09-13 07:05:16');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (4,4,'opqrs','working in MySQL','2026-09-13 07:05:48');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (5,5,'tuvwx','knitting','2026-09-13 07:06:23');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (6,1,'xzzy','computer\'s skills','2026-09-13 07:16:03');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (7,1,'qasdf','dancing','2026-09-13 07:16:36');
